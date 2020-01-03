3c5e678383702949ba87fd283cce38f0486ee74b
Lucas Kassel <44962754+LucasK4s@users.noreply.github.com>
2020-01-03 20:07:34 +0100

    Fixed Project
    Fixed bugs caused by mergin the wrong branches togehter.

---
+++ Assets/Scripts/TriggerJS.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerJS : MonoBehaviour
+{
+    public bool AlreadyPlayed = false;
+
+    void OnTriggerEnter(Collider target)
+    {
+        if (target.gameObject.tag.Equals("Player") == true && AlreadyPlayed == false)
+        {
+            FindObjectOfType<AudioManager>().Play("Jumpscare1");
+            AlreadyPlayed = true;
+        }
+    }
+}
