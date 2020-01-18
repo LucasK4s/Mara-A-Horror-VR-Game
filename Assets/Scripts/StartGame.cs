b6a2f780c5e54d09d0669f2cbbb328d63ed6ef6b
lucasr4d <lucasradke@web.de>
2020-01-18 16:21:31 +0100

    MainMenu
    Added simple start-up-menu with a script that should load the "Apartment" scene when the GrabPinch trigger is pressed

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    public SteamVR_Input_Sources handType;
+    public SteamVR_Action_Boolean grabAction;
+
+    private bool CheckGrab()
+    {
+        return grabAction.GetState(handType);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CheckGrab())
+        {
+            SceneManager.LoadScene("Apartment");
+        }
+    }
+}
