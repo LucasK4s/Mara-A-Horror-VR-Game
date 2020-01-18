5d7e94a1ace3cd5ca809fb08675b1a0fd04abf20
lucasr4d <lucasradke@web.de>
2020-01-18 16:56:25 +0100

    Added Pause Menu Script
    just added a script that needs to be tested before further coding

---
+++ Assets/Scripts/PauseGame.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class PauseGame : MonoBehaviour
+{
+    public bool gamePaused = false;
+    public GameObject pauseMenu;
+    public SteamVR_Action_Boolean pauseAction;
+    public SteamVR_Input_Sources handType;
+
+    private bool CheckPauseButton()
+    {
+        return pauseAction.GetState(handType);
+    }
+
+    void Update ()
+    {
+        if (CheckPauseButton())
+        {
+            if (gamePaused == false)
+            {
+                Time.timeScale = 0;
+                gamePaused = true;
+                pauseMenu.SetActive(true);
+            }
+            else
+            {
+                pauseMenu.SetActive(false);
+                gamePaused = false;
+                Time.timeScale = 1;
+            }
+        }
+    }
+}
