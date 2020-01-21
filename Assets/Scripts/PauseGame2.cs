64b438149082145a1157311e05047fd7d36d2f4f
lucasr4d <lucasradke@web.de>
2020-01-21 15:03:00 +0100

    Alternatives Pause script hinzugefügt

---
+++ Assets/Scripts/PauseGame2.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class PauseGame2 : MonoBehaviour
+{
+    public bool gamePaused = false;
+    public GameObject pauseMenu;
+    public SteamVR_Action_Boolean pauseAction;
+    public SteamVR_Input_Sources handType;
+
+    private bool CheckPauseButton()
+    {
+        return pauseAction.GetState(handType);
+    }
+
+    void Start()
+    {
+        StartCoroutine(PauseCoroutine());
+    }
+
+    IEnumerator PauseCoroutine()
+    {   while (true)
+        {
+            if (CheckPauseButton())
+            {
+                if (gamePaused == false)
+                {
+                    Time.timeScale = 0;
+                    gamePaused = true;
+                    pauseMenu.SetActive(true);
+                }
+                else
+                {
+                    pauseMenu.SetActive(false);
+                    gamePaused = false;
+                    Time.timeScale = 1;
+                }
+            }
+            yield return null;
+        }
+    }
+}
