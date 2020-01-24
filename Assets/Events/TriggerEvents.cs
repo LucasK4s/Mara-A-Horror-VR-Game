3754cb72b32daa7617c0848e2045c320863b725a
Lucas Kassel <44962754+LucasK4s@users.noreply.github.com>
2020-01-24 04:52:29 +0100

    Small changes
    Replaced texture, added scripts for flickering light and bookshelf jump scare, removed script pause2

---
+++ Assets/Events/TriggerEvents.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerEvents : MonoBehaviour
+{
+    public Rigidbody trigger;
+    public GameObject destroy;
+    void OnTriggerEnter(Collider other)
+    {
+        trigger.GetComponent<Rigidbody>().AddForce(Vector3.forward*10, ForceMode.Acceleration);
+        Destroy(destroy);
+    }
+    
+}
