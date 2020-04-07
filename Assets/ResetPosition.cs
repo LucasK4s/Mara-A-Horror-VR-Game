bcaab6398fbf28da83c995f4bfe2d0525bde4f5b
lucasr4d <lucasradke@web.de>
2020-04-07 21:32:46 +0200

    implemented new door opening system on closet

---
+++ Assets/ResetPosition.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetPosition : MonoBehaviour
+{
+    public Transform target;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        rb.MovePosition(target.transform.position);
+        rb.MoveRotation(target.transform.rotation);
+    }
+}
