29fd0013e2182285d96136234f270557c21156a8
Lucas Kassel <44962754+LucasK4s@users.noreply.github.com>
2020-04-08 01:21:22 +0200

    Lucas R. Added fully functional Door opening system

---
+++ Assets/Scripts/OpenIt.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+/*
+ * This class is attached to a door handle. The door handle is child of a door.
+ */
+public class OpenIt : MonoBehaviour
+{
+    private Vector3 force;
+    private Vector3 cross;
+    private bool holdingHandle;
+    private float angle;
+    private const float forceMultiplier = 150f;
+    public SteamVR_Input_Sources handType;
+    public SteamVR_Action_Boolean grabAction;
+
+    private void HandHoverUpdate(Hand hand)
+    {
+        if (grabAction.GetState(handType))
+        {
+            holdingHandle = true;
+
+            // Direction vector from the door's pivot point to the hand's current position
+            Vector3 doorPivotToHand = hand.transform.position - transform.parent.position;
+
+            // Ignore the y axis of the direction vector
+            doorPivotToHand.y = 0;
+
+            // Direction vector from door handle to hand's current position
+            force = hand.transform.position - transform.position;
+
+            // Cross product between force and direction. 
+            cross = Vector3.Cross(doorPivotToHand, force);
+            angle = Vector3.Angle(doorPivotToHand, force);
+        }
+        else if (!grabAction.GetState(handType))
+        {
+            holdingHandle = false;
+        }
+    }
+
+    void Update()
+    {
+        if (holdingHandle)
+        {
+            // Apply cross product and calculated angle to
+            GetComponentInParent<Rigidbody>().angularVelocity = cross * angle * forceMultiplier;
+        }
+    }
+
+    private void OnHandHoverEnd()
+    {
+        // Set angular velocity to zero if the hand stops hovering
+        GetComponentInParent<Rigidbody>().angularVelocity = Vector3.zero;
+    }
+}
---
+++ Assets/Scripts/ResetAfterGrab.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class ResetAfterGrab : Valve.VR.InteractionSystem.Throwable
+{
+    public Transform handler;
+    public Transform door;
+
+    protected override void OnDetachedFromHand(Hand hand)
+    {
+        attached = false;
+
+        onDetachFromHand.Invoke();
+
+        hand.HoverUnlock(null);
+
+        rigidbody.interpolation = hadInterpolation;
+        
+        Vector3 velocity;
+        Vector3 angularVelocity;
+
+        GetReleaseVelocities(hand, out velocity, out angularVelocity);
+
+        rigidbody.velocity = Vector3.zero;
+        rigidbody.angularVelocity = Vector3.zero;
+        //_________________________________________________
+        transform.position = handler.transform.position;
+        transform.rotation = handler.transform.rotation;
+
+        Rigidbody rbhandler = handler.GetComponent<Rigidbody>();
+
+        rbhandler.velocity = Vector3.zero;
+        rbhandler.angularVelocity = Vector3.zero;
+
+
+        door.GetComponent<Rigidbody>().velocity = velocity;
+        door.GetComponent<Rigidbody>().angularVelocity = angularVelocity;
+
+    }
+}
